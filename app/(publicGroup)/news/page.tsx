84e4bb15e186c4898437d15131a065c7a4b23ff5
Masad Rayan <masadrayan2002@gmail.com>
2026-07-17 03:40:57 +0600

    feat(ui): add input and label components and update login page

--- app/(publicGroup)/news/page.tsx
+++ app/(publicGroup)/news/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from '@/components/ui/button';
 import React from 'react'
 
 const NewsPage = () => {
   return (
     <div>
-      
+        This is news page
+      <Button
+      size={"lg"}
+      variant={"default"}
+      >News</Button>
     </div>
   )
 }
