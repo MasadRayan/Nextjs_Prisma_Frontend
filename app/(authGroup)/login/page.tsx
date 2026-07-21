5c3371374ed43bdc5ea9b98f91c1faebc18ba731
Masad Rayan <masadrayan2002@gmail.com>
2026-07-21 23:51:38 +0600

    added the login from in the login page

--- app/(authGroup)/login/page.tsx
+++ app/(authGroup)/login/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LoginFrom from '../_components/LoginFrom';
 
 export default function LoginPage() {
   return (
     <>
     <div className= "flex min-h-screen items-center justify-center">
-        <div className="w-full.  max-w-md space-y-6 rounded-lg border py-8 shadow-lg">
+        <div className="w-full.  max-w-md space-y-6 rounded-lg border py-8 shadow-lg px-5">
             {/* From Generic Content */}
             <div className="space-y-2 text-center">
               <h1 className="text-3xl font-bold">Welcome Back!</h1>
@@ -12,6 +13,9 @@ export default function LoginPage() {
                 Enter your vredentials to acceess your account and continue your journey with us.
               </p>
             </div>
+
+            {/* Login Form */}
+            <LoginFrom></LoginFrom>
         </div>
     </div>
     </>
