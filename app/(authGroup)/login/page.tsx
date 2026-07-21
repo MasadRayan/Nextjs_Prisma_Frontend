30d8183e57f6c39a71918a0126e4041640736c24
Masad Rayan <masadrayan2002@gmail.com>
2026-07-21 23:39:17 +0600

    fix: correct spelling errors in login page content

--- app/(authGroup)/login/page.tsx
+++ app/(authGroup)/login/page.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 export default function LoginPage() {
   return (
     <>
+    <div className= "flex min-h-screen items-center justify-center">
+        <div className="w-full.  max-w-md space-y-6 rounded-lg border py-8 shadow-lg">
+            {/* From Generic Content */}
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl font-bold">Welcome Back!</h1>
+              <p className="text-gray-600">
+                Enter your vredentials to acceess your account and continue your journey with us.
+              </p>
+            </div>
+        </div>
+    </div>
     </>
   )
 }
