cb2ebaa9502fcf93d30043baef900adbdeb0d6ce
Masad Rayan <masadrayan2002@gmail.com>
2026-07-22 04:10:14 +0600

    added navigation in the server side

--- app/(authGroup)/_actions/loginAction.ts
+++ app/(authGroup)/_actions/loginAction.ts
@@ -1,6 +1,9 @@
 "use server"
 
-type loginPendingState = {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+type LoginState = {
     success: boolean,
     statusCode: number,
     message: string,
@@ -11,7 +14,7 @@ type loginPendingState = {
 }
 
 
-export const loginAction = async (pendingState : loginPendingState ,formData : FormData) => {
+export const loginAction = async (pendingState : LoginState ,formData : FormData) => {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
     const body = {
@@ -27,6 +30,26 @@ export const loginAction = async (pendingState : loginPendingState ,formData : F
         body: JSON.stringify(body)
     });
 
-    const result = await res.json();
+    
+    const result  = await res.json();
+
+    if (result.success) {
+        const cookieStore = await cookies();
+
+        cookieStore.set("accessToken", result.data.accessToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24, // 1 day
+            sameSite: "lax"
+        })
+
+        cookieStore.set("refreshToken", result.data.refreshToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7, // 1 week
+            sameSite: "lax"
+        })
+
+        redirect("/dashboard")
+    }
+
     return result
 }
