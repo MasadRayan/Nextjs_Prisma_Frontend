6102519bd766e38df7f9130e55de5787a42684c4
Masad Rayan <masadrayan2002@gmail.com>
2026-07-27 23:13:32 +0600

    feat(dashboard): enhance My Posts page with loading skeleton and improved layout
    - Added a loading skeleton component (MyPostSkeleton) for better user experience during data fetching.
    - Updated the My Posts page layout with a centered title and description for improved readability.
    - Introduced a new MyPostList component for future implementation of post listing.
    - Created reusable UI components including Checkbox, Dialog, Pagination, Sidebar, Skeleton, Textarea, and Tooltip.
    - Implemented a custom hook (useIsMobile) to manage mobile responsiveness.

--- app/(dashboardGroup)/dashboard/my-posts/page.tsx
+++ app/(dashboardGroup)/dashboard/my-posts/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import MyPostSkeleton from '../../_components/myPostComponent/MyPostSkeleton';
 
 const userMyPostPage = () => {
   return (
-    <div>
-      
+    <div className= " mx-auto max-w-7xl space-y-6 px-4 py-10 sm:px-6 lg:px-8">
+      <div className="z-10 max-w-lg text-center mx-auto">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mb-4">My Posts</h1>
+        <p className="text-muted-foreground">
+          Create and manage your posts in one place. Here, you can view all your published and draft posts, edit them, and keep track of their performance. Stay organized and make the most out of your content creation journey.
+        </p>
+      </div>
+
+      <Suspense fallback={<MyPostSkeleton />}>
+
+      </Suspense>
+
     </div>
   )
 }
